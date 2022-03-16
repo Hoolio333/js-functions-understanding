24cd34f80991be4dcf2f927e3024f67357739b75
Chris Hyde <c_hyde333@hotmail.com>
2022-03-16 12:17:17 +0000

    Q1 - Done

---
+++ q1.js
@@ -0,0 +1,18 @@
+function myFunction(num1, num2) {
+  return num1+num2
+}
+
+const result = myFunction(5,5)
+
+console.log(myFunction())
+console.log(result)
+
+// The code has a function called myFunction & it passes two arguments within it (num1 & num2)
+
+// The function wants to return num1 + num2
+
+// There's a const variable for the outcome called result.  This result has the outcome of the function and shows the values in brackets of num1 (5) & num2 (5)
+
+// Prediction - shows the function arguments - num1, num2, then below shows 10.
+
+// Outcome showed "Nan" & "10" - The result part was as expected.  The function doesn't show the argument names just NaN as there's no values so nothing to show.  
